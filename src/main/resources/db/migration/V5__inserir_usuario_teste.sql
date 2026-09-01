c1155bc633140a56d73979710755c0579b8d56f6
Pedro Aurélio <pedroaurelio.reis@gmail.com>
2026-09-01 16:34:15 -0300

    feat(auth): configurar cors, atualizar porta db e add usuario de testes

---
+++ src/main/resources/db/migration/V5__inserir_usuario_teste.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario (id, nome, email, senha, papel) 
+VALUES ('c902c114-1188-467a-8b1b-b46132717088', 'Porteiro de Teste', 'porteiro@condominio.com', '$2a$10$32llGM2RkI4wgwTbez56cuKZ6h9gC/u2eNcOjuXFsqdWkrlIKrSEq', 'ROLE_PORTEIRO');
